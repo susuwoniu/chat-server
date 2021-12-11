16dfdb0e3fbb00b2d2fbdbd38837b2e37056ff18
True <true@communication.com>
2021-12-11 21:21:18 +0800

    fix: posts after null

--- server/src/util/base62_to_i64.rs
+++ server/src/util/base62_to_i64.rs
@@ -4,15 +4,21 @@ use crate::{
   types::ServiceResult,
 };
 pub fn base62_to_i64(base62: &str) -> ServiceResult<i64> {
-  return Ok(
-    bs62::decode_num(base62)
-      .map_err(|_| {
-        ServiceError::param_invalid(
-          &Locale::default(),
-          "parse_base62_to_i64_failed",
-          Error::Default,
-        )
-      })?
-      .to_u64_digits()[0] as i64,
-  );
+  if base62.len() == 0 {
+    return Err(ServiceError::param_invalid(
+      &Locale::default(),
+      "parse_base62_to_i64_failed",
+      Error::Default,
+    ));
+  }
+  let result = bs62::decode_num(base62)
+    .map_err(|_| {
+      ServiceError::param_invalid(
+        &Locale::default(),
+        "parse_base62_to_i64_failed",
+        Error::Default,
+      )
+    })?
+    .to_u64_digits()[0] as i64;
+  return Ok(result);
 }
