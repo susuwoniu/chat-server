a7e1f9b2c67945d0ae431b5875ac1ccfd6f49d38
True <true@communication.com>
2021-12-26 16:02:48 +0800

    fix: extension

--- migrations/20211226022741_psot_location.sql
+++ migrations/20211226022741_psot_location.sql
@@ -1,6 +1,5 @@
 -- Add migration script here
-create extension postgis;
-
+CREATE EXTENSION IF NOT EXISTS postgis;
 ALTER TABLE posts
   ADD COLUMN geom geometry(point, 4326);
 create index posts_location_index_geom on posts using gist(geom);
